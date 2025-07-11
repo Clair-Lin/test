83e8b4466045f6268b8cca1681c6f21459a25c07
Clair-Lin <zengjiehe@163.com>
2025-07-11 12:56:12 +0800

    更新

--- src/main.js
+++ src/main.js
@@ -3,9 +3,6 @@ import './style.css'
 import App from './App.vue'
 import router from './router'
 
-createApp(App).use(router).mount('#app')
-
-// 在 main.js 中添加全局错误处理
-app.config.errorHandler = (err) => {
-    console.error('Vue error:', err)
-  }
+const app = createApp(App)
+app.use(router)
+app.mount('#app')
